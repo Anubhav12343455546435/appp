13eabea8fb616ae3f68073223346b0203144a3b8
Anubhav12343455546435 <140024389+Anubhav12343455546435@users.noreply.github.com>
2023-11-01 22:57:28 -0400

    dothis

---
+++ Product/app/config/db.config.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+
+
+module.exports = {
+
+  
+
+url: "mongodb+srv://anubhavpandit391:123456AP@cluster0.vhpcpxj.mongodb.net/Ap?retryWrites=true&w=majority"
+
+};
